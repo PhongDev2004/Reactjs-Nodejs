3572b2d902103209e3ba6b1fc3c1fcdf46db6b1b
Chi Viet <chiviet296@gmail.com>
2024-06-28 22:26:25 +0700

    fix upload image

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,6 +23,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
 
 app.use('/api/auth', userRouter);
 app.use('/api/products', productRouter);
--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -7,18 +7,22 @@ import {
 	updateProduct,
 } from '../controllers/productController';
 import { protect, restrictTo } from '../controllers/authController';
+import multer from 'multer';
 
 const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage, limits: { fileSize: 1024 * 1024 * 5 } });
 
 router
 	.route('/')
 	.get(getAllProducts)
-	.post(protect, restrictTo('admin'), createProduct);
+	.post(upload.single('image'), protect, restrictTo('admin'), createProduct);
 
 router
 	.route('/:id')
 	.get(getProduct)
-	.patch(protect, restrictTo('admin'), updateProduct)
+	.patch(upload.single('image'), protect, restrictTo('admin'), updateProduct)
 	.delete(protect, restrictTo('admin'), deleteProduct);
 
 export default router;
