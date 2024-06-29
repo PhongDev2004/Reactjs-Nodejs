9f66d014a8fd11f0e1769be26cc7cd5428c6496a
Chi Viet <chiviet296@gmail.com>
2024-06-29 13:36:05 +0700

    cart frontend

---
+++ frontend/src/service/cart.ts
@@ -0,0 +1,46 @@
+import instance from './api';
+
+export const getCart = async () => {
+	try {
+		const { data } = await instance.get('/cart');
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const addToCart = async (productId: string, quantity: number) => {
+	try {
+		const { data } = await instance.post('/cart', {
+			productId,
+			quantity,
+		});
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const removeFromCart = async (productId: string | undefined) => {
+	try {
+		const { data } = await instance.patch(`/cart/${productId}`);
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const updateCart = async (
+	productId: string | undefined,
+	quantity: number
+) => {
+	try {
+		const { data } = await instance.patch(`/cart`, {
+			quantity,
+			productId,
+		});
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+};
